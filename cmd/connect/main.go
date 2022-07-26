afeffb943413ae534bb037a5dd24ac9ea9dba50a
moreirathomas <moreirathomas97@gmail.com>
2022-07-26 19:41:22 +0200

    feat: single socket with dynamic event handling

--- cmd/connect/main.go
+++ cmd/connect/main.go
@@ -25,23 +25,70 @@ func main() {
 
 	fmt.Printf("listening on http://%s\n", addr)
 
-	http.HandleFunc("/run", run)
-	http.HandleFunc("/status", status)
+	http.HandleFunc("/run", handleRun)
 
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
-func run(w http.ResponseWriter, r *http.Request) {
+type Runner struct {
+	running  bool
+	progress progress
+
+	isPollingProgress    bool
+	abortPollingProgress chan struct{}
+}
+
+func (r *Runner) run() string {
+	if !r.running {
+		r.running = true
+		r.progress = progress{}
+
+		// Prepare abort signal.
+		r.abortPollingProgress = make(chan struct{}, 1)
+
+		return "running"
+	}
+
+	return "error: already running"
+}
+
+func (r *Runner) stop() string {
+	if !r.running {
+		return "error: not running"
+	}
+
+	r.running = false
+	r.abortPollingProgress <- struct{}{}
+	r.isPollingProgress = false
+	r.progress = progress{}
+
+	return "stopped"
+}
+
+type progress struct {
+	value int
+}
+
+func (p *progress) next() int {
+	// Go no further than 100.
+	if p.value <= 100 {
+		p.value++
+	}
+
+	return p.value
+}
+
+func handleRun(w http.ResponseWriter, r *http.Request) {
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	log.Println("connected /run")
+	log.Printf("websocket connected with client %s", r.Host)
 
 	defer ws.Close()
 
-	dr := dummyRun{}
+	runner := Runner{}
 
 	for {
 		m, err := readTextMessage(ws)
@@ -50,36 +97,62 @@ func run(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 
+		log.Printf("<- %s", m)
+
 		switch string(m) {
 		case "run":
-			if dr.started {
-				log.Println("already started")
+			status := runner.run()
 
-				err = ws.WriteMessage(websocket.TextMessage, []byte("already started"))
-				if err != nil {
-					log.Println("cannot write message:", err)
-					break
-				}
-			} else {
-				log.Println("starting run")
-
-				dr.run()
-
-				err = ws.WriteMessage(websocket.TextMessage, []byte("ack"))
-				if err != nil {
-					log.Println("cannot write message:", err)
-					break
-				}
-			}
-
-		default:
-			log.Printf("<- %s", m)
-
-			err = ws.WriteMessage(websocket.TextMessage, m)
+			err = writeTextMessage(ws, status)
 			if err != nil {
-				log.Println("cannot write message:", err)
 				break
 			}
+
+		case "stop":
+			status := runner.stop()
+
+			err = writeTextMessage(ws, status)
+			if err != nil {
+				break
+			}
+
+		case "pull":
+			err = writeTextMessage(ws, "not implemented")
+			if err != nil {
+				break
+			}
+			log.Println("^^ not implemented", m)
+		default:
+			log.Println("^^ not implemented", m)
+		}
+
+		if runner.running && !runner.isPollingProgress {
+			go runner.pollProgress(ws)
+		}
+	}
+}
+
+func (r *Runner) pollProgress(ws *websocket.Conn) {
+	r.isPollingProgress = true
+
+	for range time.Tick(time.Millisecond * 500) {
+		select {
+		case <-r.abortPollingProgress:
+			return
+		default:
+		}
+
+		if r.progress.value == 100 {
+			r.isPollingProgress = false
+			return
+		}
+
+		val := r.progress.next()
+		percent := fmt.Sprintf("%d%%", val)
+
+		err := writeTextMessage(ws, percent)
+		if err != nil {
+			break
 		}
 	}
 }
@@ -97,43 +170,14 @@ func readTextMessage(ws *websocket.Conn) ([]byte, error) {
 	return m, nil
 }
 
-type dummyRun struct {
-	started bool
-}
-
-func (d *dummyRun) run() {
-	d.started = true
-}
-
-func status(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
+func writeTextMessage(ws *websocket.Conn, m string) error {
+	err := ws.WriteMessage(websocket.TextMessage, []byte(m))
 	if err != nil {
-		log.Println(err)
-		return
+		log.Println("cannot write message:", err)
+		return err
 	}
-	log.Println("connected /status")
 
-	defer ws.Close()
+	log.Printf("-> %s", m)
 
-	ds := dummyStatus{}
-
-	for range time.Tick(time.Second * 1) {
-		ds.inc()
-		data := []byte(fmt.Sprint(ds.c))
-
-		err = ws.WriteMessage(websocket.TextMessage, data)
-		if err != nil {
-			log.Println("cannot write message:", err)
-			break
-		}
-		log.Printf("-> %s", data)
-	}
-}
-
-type dummyStatus struct {
-	c int
-}
-
-func (d *dummyStatus) inc() {
-	d.c++
+	return nil
 }
