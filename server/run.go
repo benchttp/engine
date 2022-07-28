f12ec2f68c3117f1833605732f6e146ab13756e1
moreirathomas <moreirathomas97@gmail.com>
2022-07-28 11:01:10 +0200

    chore: rename progress method to match standard

--- server/run.go
+++ server/run.go
@@ -29,7 +29,7 @@ func (r *run) start(w socketio.Writer) {
 	ctx, cancel := context.WithCancel(context.Background())
 	r.cancel = cancel
 
-	r.runner = runner.New(r.sendProgess(w))
+	r.runner = runner.New(r.sendRecordingProgess(w))
 
 	out, err := r.runner.Run(ctx, config())
 	if err != nil {
@@ -52,11 +52,11 @@ func (r *run) stop() bool {
 	return true
 }
 
-// sendProgress sends the current runner.RecordingProgress via w.
-// As multiple goroutines may invoke sendProgess simultaneously
+// sendRecordingProgess sends the current runner.RecordingProgress via w.
+// As multiple goroutines may invoke run.sendRecordingProgess concurrently
 // as a callback from runner.onRecordingProgress, writing to w
 // is protected by a lock.
-func (r *run) sendProgess(w socketio.Writer) func(runner.RecordingProgress) {
+func (r *run) sendRecordingProgess(w socketio.Writer) func(runner.RecordingProgress) {
 	return func(rp runner.RecordingProgress) {
 		r.mu.Lock()
 		defer r.mu.Unlock()
