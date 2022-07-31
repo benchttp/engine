6ce2447e7fc06726d6e653aa8f576e273d9b7c99
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-07-31 22:35:29 +0200

    test(tests): implement TestRun

--- runner/internal/tests/tests_test.go
+++ runner/internal/tests/tests_test.go
@@ -10,35 +10,105 @@ import (
 )
 
 func TestRun(t *testing.T) {
-	t.Run("happy path", func(_ *testing.T) {
-		agg := metricsStub()
-		queries := []tests.Case{
-			{
-				Name:      "minimum response time is 50ms", // succeeding case
-				Source:    metrics.ResponseTimeMax,
-				Predicate: tests.GT,
-				Target:    metrics.Value(50 * time.Millisecond),
+	testcases := []struct {
+		label          string
+		inputAgg       metrics.Aggregate
+		inputCases     []tests.Case
+		expGlobalPass  bool
+		expCaseResults []tests.CaseResult
+	}{
+		{
+			label:    "pass if all cases pass",
+			inputAgg: metrics.Aggregate{Avg: 100 * time.Millisecond},
+			inputCases: []tests.Case{
+				{
+					Name:      "average response time below 120ms (pass)",
+					Predicate: tests.LT,
+					Source:    metrics.ResponseTimeAvg,
+					Target:    120 * time.Millisecond,
+				},
+				{
+					Name:      "average response time is above 80ms (pass)",
+					Predicate: tests.GT,
+					Source:    metrics.ResponseTimeAvg,
+					Target:    80 * time.Millisecond,
+				},
 			},
-			{
-				Name:      "maximum response time is 110ms", // failing case
-				Source:    metrics.ResponseTimeMax,
-				Predicate: tests.LT,
-				Target:    metrics.Value(110 * time.Millisecond),
+			expGlobalPass: true,
+			expCaseResults: []tests.CaseResult{
+				{Pass: true, Summary: "want AVG < 120ms, got 100ms"},
+				{Pass: true, Summary: "want AVG > 80ms, got 100ms"},
 			},
-		}
-		result := tests.Run(agg, queries)
-		fmt.Printf("%+v\n", result)
-	})
+		},
+		{
+			label:    "fail if at least one case fails",
+			inputAgg: metrics.Aggregate{Avg: 200 * time.Millisecond},
+			inputCases: []tests.Case{
+				{
+					Name:      "average response time below 120ms (fail)",
+					Predicate: tests.LT,
+					Source:    metrics.ResponseTimeAvg,
+					Target:    120 * time.Millisecond,
+				},
+				{
+					Name:      "average response time is above 80ms (pass)",
+					Predicate: tests.GT,
+					Source:    metrics.ResponseTimeAvg,
+					Target:    80 * time.Millisecond,
+				},
+			},
+			expGlobalPass: false,
+			expCaseResults: []tests.CaseResult{
+				{Pass: false, Summary: "want AVG < 120ms, got 200ms"},
+				{Pass: true, Summary: "want AVG > 80ms, got 200ms"},
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.label, func(t *testing.T) {
+			suiteResult := tests.Run(tc.inputAgg, tc.inputCases)
+
+			if gotGlobalPass := suiteResult.Pass; gotGlobalPass != tc.expGlobalPass {
+				t.Errorf(
+					"exp global pass == %v, got %v",
+					gotGlobalPass, tc.expGlobalPass,
+				)
+			}
+
+			assertEqualCaseResults(t, tc.expCaseResults, suiteResult.Results)
+		})
+	}
 }
 
-func metricsStub() metrics.Aggregate {
-	return metrics.Aggregate{
-		Min: 80 * time.Millisecond,
-		Max: 120 * time.Millisecond,
-		Avg: 100 * time.Millisecond,
+func assertEqualCaseResults(t *testing.T, exp, got []tests.CaseResult) {
+	t.Helper()
 
-		TotalCount:   10,
-		FailureCount: 1,
-		SuccessCount: 9,
+	if gotLen, expLen := len(got), len(exp); gotLen != expLen {
+		t.Errorf("exp %d case results, got %d", expLen, gotLen)
+	}
+
+	for i, expResult := range exp {
+		gotResult := got[i]
+		caseDesc := fmt.Sprintf("case #%d (%q)", i, gotResult.Input.Name)
+
+		t.Run(fmt.Sprintf("cases[%d].Pass", i), func(t *testing.T) {
+			if gotResult.Pass != expResult.Pass {
+				t.Errorf(
+					"\n%s:\nexp %v, got %v",
+					caseDesc, expResult.Pass, gotResult.Pass,
+				)
+			}
+		})
+
+		t.Run(fmt.Sprintf("cases[%d].Summary", i), func(t *testing.T) {
+			if gotResult.Summary != expResult.Summary {
+				t.Errorf(
+					"\n%s:\nexp %q\ngot %q",
+					caseDesc, expResult.Summary, gotResult.Summary,
+				)
+			}
+		})
+
 	}
 }
