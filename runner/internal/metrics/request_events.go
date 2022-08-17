e0e672875042725853f9714b16d17bf952098232
C <>
2022-08-17 23:15:32 +0200

    Reformat computeRequestEventTimes by adding another function

--- runner/internal/metrics/request_events.go
+++ runner/internal/metrics/request_events.go
@@ -10,18 +10,10 @@ import (
 func computeRequestEventTimes(records []recorder.Record) (requestEventTimes map[string]timestats.TimeStats) {
 	requestEventTimes = make(map[string]timestats.TimeStats, 0)
 
-	var allEvents []recorder.Event
-	for _, record := range records {
-		for i, event := range record.Events {
-			if i > 0 {
-				event = recorder.Event{Name: event.Name, Time: event.Time - record.Events[i-1].Time}
-			}
-			allEvents = append(allEvents, event)
-		}
-	}
+	allEventsWithTime := extractAllEventsWithTime(records)
 
 	EachEventWithTimes := make(map[string][]time.Duration, 0)
-	for _, event := range allEvents {
+	for _, event := range allEventsWithTime {
 		EachEventWithTimes[event.Name] = append(EachEventWithTimes[event.Name], event.Time)
 	}
 
@@ -30,4 +22,19 @@ func computeRequestEventTimes(records []recorder.Record) (requestEventTimes map[
 	}
 
 	return requestEventTimes
+}
+
+// extractAllEventsWithTime gets all events in all records of a slice of records, with their time.
+// It takes care of calculating the time of each event, as opposed to
+// the time since the beginning of the request as it appears in the records.
+func extractAllEventsWithTime(records []recorder.Record) (allEventsWithTime []recorder.Event) {
+	for _, record := range records {
+		for i, event := range record.Events {
+			if i > 0 {
+				event = recorder.Event{Name: event.Name, Time: event.Time - record.Events[i-1].Time}
+			}
+			allEventsWithTime = append(allEventsWithTime, event)
+		}
+	}
+	return allEventsWithTime
 }
