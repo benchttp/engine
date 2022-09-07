754bef6f03968150e611899bf7309d7f403b7ddc
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-09-07 20:23:42 +0200

    test(metrics): write unit tests for Field.Validate

--- runner/internal/metrics/field_test.go
+++ runner/internal/metrics/field_test.go
@@ -35,8 +35,46 @@ func TestField_Type(t *testing.T) {
 	}
 
 	for _, c := range cases {
-		if got := metrics.Field(c.fieldID).Type(); got != c.exp {
-			t.Errorf("bad type: exp %s, got %s", c.exp, got)
-		}
+		t.Run(c.name, func(t *testing.T) {
+			if got := metrics.Field(c.fieldID).Type(); got != c.exp {
+				t.Errorf("bad type: exp %s, got %s", c.exp, got)
+			}
+		})
+	}
+}
+
+func TestField_Validate(t *testing.T) {
+	cases := []struct {
+		name     string
+		fieldID  string
+		expError string
+	}{
+		{
+			name:     "valid field",
+			fieldID:  "ResponseTimes.Mean",
+			expError: "",
+		},
+		{
+			name:     "invalid field",
+			fieldID:  "Marcel.Patulacci",
+			expError: "metrics: unknown field: Marcel.Patulacci",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			shouldError := c.expError != ""
+			err := metrics.Field(c.fieldID).Validate()
+			if shouldError {
+				if err == nil {
+					t.Error("expect non-nil error, got nil")
+				}
+				if got := err.Error(); got != c.expError {
+					t.Errorf("exp %q\ngot %q", c.expError, got)
+				}
+			} else if err != nil {
+				t.Errorf("unexpected error: %q", err.Error())
+			}
+		})
 	}
 }
