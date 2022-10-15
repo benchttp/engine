0c7243bb26e8ea581de56a36c5357f11163c5f80
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-10-15 18:19:20 +0200

    refactor(configparse): implement Representation.Unmarshal
    - ParseRepresentation -> UnmarshalRepresentation: change signature
    so it matches json.Unmarshal style
    - implement Representation.Unmarshal: alias to UnmarshalRepresentation
    - update JSON accordingly

--- configparse/parse.go
+++ configparse/parse.go
@@ -44,32 +44,27 @@ type Representation struct {
 	} `yaml:"tests" json:"tests"`
 }
 
-// ParseRepresentation parses an input raw config as a runner.ConfigGlobal and returns
-// a parsed Config or the first non-nil error occurring in the process.
-func ParseRepresentation(repr Representation) (runner.Config, error) { //nolint:gocognit // acceptable complexity for a parsing func
-	cfg := runner.Config{}
-	assignedFields := []string{}
+// Unmarshal parses the Representation receiver as a runner.Config
+// and stores any non-nil field value into the corresponding field
+// of dst.
+func (repr Representation) Unmarshal(dst *runner.Config) error {
+	return UnmarshalRepresentation(repr, dst)
+}
 
-	addField := func(field string) {
-		assignedFields = append(assignedFields, field)
-	}
-
-	abort := func(err error) (runner.Config, error) {
-		return runner.Config{}, err
-	}
-
+// Unmarshal parses the given Representation as a runner.Config
+// and stores any non-nil field value into the corresponding field
+// of dst.
+func UnmarshalRepresentation(repr Representation, cfg *runner.Config) error { //nolint:gocognit // acceptable complexity for a parsing func
 	if method := repr.Request.Method; method != nil {
 		cfg.Request.Method = *method
-		addField(runner.ConfigFieldMethod)
 	}
 
 	if rawURL := repr.Request.URL; rawURL != nil {
-		parsedURL, err := parseAndBuildURL(*repr.Request.URL, repr.Request.QueryParams)
+		parsedURL, err := parseAndBuildURL(*rawURL, repr.Request.QueryParams)
 		if err != nil {
-			return abort(err)
+			return err
 		}
 		cfg.Request.URL = parsedURL
-		addField(runner.ConfigFieldURL)
 	}
 
 	if header := repr.Request.Header; header != nil {
@@ -78,7 +73,6 @@ func ParseRepresentation(repr Representation) (runner.Config, error) { //nolint:
 			httpHeader[key] = val
 		}
 		cfg.Request.Header = httpHeader
-		addField(runner.ConfigFieldHeader)
 	}
 
 	if body := repr.Request.Body; body != nil {
@@ -86,49 +80,43 @@ func ParseRepresentation(repr Representation) (runner.Config, error) { //nolint:
 			Type:    body.Type,
 			Content: []byte(body.Content),
 		}
-		addField(runner.ConfigFieldBody)
 	}
 
 	if requests := repr.Runner.Requests; requests != nil {
 		cfg.Runner.Requests = *requests
-		addField(runner.ConfigFieldRequests)
 	}
 
 	if concurrency := repr.Runner.Concurrency; concurrency != nil {
 		cfg.Runner.Concurrency = *concurrency
-		addField(runner.ConfigFieldConcurrency)
 	}
 
 	if interval := repr.Runner.Interval; interval != nil {
 		parsedInterval, err := parseOptionalDuration(*interval)
 		if err != nil {
-			return abort(err)
+			return err
 		}
 		cfg.Runner.Interval = parsedInterval
-		addField(runner.ConfigFieldInterval)
 	}
 
 	if requestTimeout := repr.Runner.RequestTimeout; requestTimeout != nil {
 		parsedTimeout, err := parseOptionalDuration(*requestTimeout)
 		if err != nil {
-			return abort(err)
+			return err
 		}
 		cfg.Runner.RequestTimeout = parsedTimeout
-		addField(runner.ConfigFieldRequestTimeout)
 	}
 
 	if globalTimeout := repr.Runner.GlobalTimeout; globalTimeout != nil {
 		parsedGlobalTimeout, err := parseOptionalDuration(*globalTimeout)
 		if err != nil {
-			return abort(err)
+			return err
 		}
 		cfg.Runner.GlobalTimeout = parsedGlobalTimeout
-		addField(runner.ConfigFieldGlobalTimeout)
 	}
 
 	testSuite := repr.Tests
 	if len(testSuite) == 0 {
-		return cfg.WithFields(assignedFields...), nil
+		return nil
 	}
 
 	cases := make([]runner.TestCase, len(testSuite))
@@ -143,22 +131,22 @@ func ParseRepresentation(repr Representation) (runner.Config, error) { //nolint:
 			fieldPath("predicate"): t.Predicate,
 			fieldPath("target"):    t.Target,
 		}); err != nil {
-			return abort(err)
+			return err
 		}
 
 		field := runner.MetricsField(*t.Field)
 		if err := field.Validate(); err != nil {
-			return abort(fmt.Errorf("%s: %s", fieldPath("field"), err))
+			return fmt.Errorf("%s: %s", fieldPath("field"), err)
 		}
 
 		predicate := runner.TestPredicate(*t.Predicate)
 		if err := predicate.Validate(); err != nil {
-			return abort(fmt.Errorf("%s: %s", fieldPath("predicate"), err))
+			return fmt.Errorf("%s: %s", fieldPath("predicate"), err)
 		}
 
 		target, err := parseMetricValue(field, fmt.Sprint(t.Target))
 		if err != nil {
-			return abort(fmt.Errorf("%s: %s", fieldPath("target"), err))
+			return fmt.Errorf("%s: %s", fieldPath("target"), err)
 		}
 
 		cases[i] = runner.TestCase{
@@ -169,9 +157,8 @@ func ParseRepresentation(repr Representation) (runner.Config, error) { //nolint:
 		}
 	}
 	cfg.Tests = cases
-	addField(runner.ConfigFieldTests)
 
-	return cfg.WithFields(assignedFields...), nil
+	return nil
 }
 
 // helpers
