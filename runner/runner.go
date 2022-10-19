d8c2e7789e982b8c4d77e9374d8cad73d5ba61f0
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-10-19 03:19:50 +0200

    refactor(runner): nuke Config, flatten Runner fields

--- runner/runner.go
+++ runner/runner.go
@@ -2,6 +2,9 @@ package runner
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/benchttp/engine/runner/internal/metrics"
@@ -34,6 +37,16 @@ const (
 var ErrCanceled = recorder.ErrCanceled
 
 type Runner struct {
+	Request *http.Request
+
+	Requests       int
+	Concurrency    int
+	Interval       time.Duration
+	RequestTimeout time.Duration
+	GlobalTimeout  time.Duration
+
+	Tests []tests.Case
+
 	recorder            *recorder.Recorder
 	onRecordingProgress func(RecordingProgress)
 }
@@ -42,14 +55,14 @@ func New(onRecordingProgress func(RecordingProgress)) *Runner {
 	return &Runner{onRecordingProgress: onRecordingProgress}
 }
 
-func (r *Runner) Run(ctx context.Context, cfg Config) (*Report, error) {
+func (r *Runner) Run(ctx context.Context, cfg Runner) (*Report, error) {
 	// Validate input config
 	if err := cfg.Validate(); err != nil {
 		return nil, err
 	}
 
 	// Create and attach request recorder
-	r.recorder = recorder.New(recorderConfig(cfg, r.onRecordingProgress))
+	r.recorder = recorder.New(r.recorderConfig())
 
 	startTime := time.Now()
 
@@ -79,16 +92,55 @@ func (r *Runner) Progress() RecordingProgress {
 }
 
 // recorderConfig returns a runner.RequesterConfig generated from cfg.
-func recorderConfig(
-	cfg Config,
-	onRecordingProgress func(recorder.Progress),
-) recorder.Config {
+func (r *Runner) recorderConfig() recorder.Config {
 	return recorder.Config{
-		Requests:       cfg.Runner.Requests,
-		Concurrency:    cfg.Runner.Concurrency,
-		Interval:       cfg.Runner.Interval,
-		RequestTimeout: cfg.Runner.RequestTimeout,
-		GlobalTimeout:  cfg.Runner.GlobalTimeout,
-		OnProgress:     onRecordingProgress,
+		Requests:       r.Requests,
+		Concurrency:    r.Concurrency,
+		Interval:       r.Interval,
+		RequestTimeout: r.RequestTimeout,
+		GlobalTimeout:  r.GlobalTimeout,
+		OnProgress:     r.onRecordingProgress,
 	}
+}
+
+// Validate returns a non-nil InvalidConfigError if any of its fields
+// does not meet the requirements.
+func (r Runner) Validate() error { //nolint:gocognit
+	errs := []error{}
+	appendError := func(err error) {
+		errs = append(errs, err)
+	}
+
+	if r.Request == nil {
+		appendError(errors.New("unexpected nil request"))
+	}
+
+	if r.Requests < 1 && r.Requests != -1 {
+		appendError(fmt.Errorf("requests (%d): want >= 0", r.Requests))
+	}
+
+	if r.Concurrency < 1 || r.Concurrency > r.Requests {
+		appendError(fmt.Errorf(
+			"concurrency (%d): want > 0 and <= requests (%d)",
+			r.Concurrency, r.Requests,
+		))
+	}
+
+	if r.Interval < 0 {
+		appendError(fmt.Errorf("interval (%d): want >= 0", r.Interval))
+	}
+
+	if r.RequestTimeout < 1 {
+		appendError(fmt.Errorf("requestTimeout (%d): want > 0", r.RequestTimeout))
+	}
+
+	if r.GlobalTimeout < 1 {
+		appendError(fmt.Errorf("globalTimeout (%d): want > 0", r.GlobalTimeout))
+	}
+
+	if len(errs) > 0 {
+		return &InvalidRunnerError{errs}
+	}
+
+	return nil
 }
