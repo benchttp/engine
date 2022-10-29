88eab2431516920e18e6e27c5a8721168086f812
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-10-29 18:03:02 +0200

    test(configio): refactor TestFindFile, TestUnmarshalFile
    - make testdata more robust easy to reason about using a dedicated
      package
    - use table testing
    - use benchttptest for runner comparison
    - extract assertXxx helpers

--- configio/file_test.go
+++ configio/file_test.go
@@ -1,94 +1,98 @@
 package configio_test
 
 import (
-	"bytes"
 	"errors"
-	"fmt"
-	"net/http"
 	"net/http/httptest"
-	"net/url"
-	"path/filepath"
-	"reflect"
 	"testing"
 	"time"
 
 	"github.com/benchttp/sdk/benchttp"
+	"github.com/benchttp/sdk/benchttptest"
 	"github.com/benchttp/sdk/configio"
+	"github.com/benchttp/sdk/configio/testdata"
 )
 
 func TestFindFile(t *testing.T) {
 	var (
-		fileYAML = configPath("valid/benchttp.yaml")
-		fileJSON = configPath("valid/benchttp.json")
-		nofile   = configPath("does-not-exist.json")
+		fileYAML = testdata.ValidFullYAML().Path
+		fileJSON = testdata.ValidFullJSON().Path
+		nofile   = testdata.InvalidPath().Path
 	)
 
-	t.Run("return first existing file from input", func(t *testing.T) {
-		files := []string{nofile, fileYAML, fileJSON}
+	testcases := []struct {
+		name         string
+		inputPaths   []string
+		defaultPaths []string
+		exp          string
+	}{
+		{
+			name:         "return first existing input path",
+			inputPaths:   []string{nofile, fileYAML, fileJSON},
+			defaultPaths: []string{},
+			exp:          fileYAML,
+		},
+		{
+			name:         "return first existing default path if no input",
+			inputPaths:   []string{},
+			defaultPaths: []string{nofile, fileYAML, fileJSON},
+			exp:          fileYAML,
+		},
+		{
+			name:         "return empty string if no file found",
+			inputPaths:   []string{nofile},
+			defaultPaths: []string{},
+			exp:          "",
+		},
+	}
 
-		if got := configio.FindFile(files...); got != fileYAML {
-			t.Errorf("did not retrieve good file: exp %s, got %s", fileYAML, got)
-		}
-	})
-
-	t.Run("return first existing file from defaults", func(t *testing.T) {
-		configio.DefaultPaths = []string{nofile, fileYAML, fileJSON}
-
-		if got := configio.FindFile(); got != fileYAML {
-			t.Errorf("did not retrieve good file: exp %s, got %s", fileYAML, got)
-		}
-	})
-
-	t.Run("return empty string when no match", func(t *testing.T) {
-		files := []string{nofile}
-
-		if got := configio.FindFile(files...); got != "" {
-			t.Errorf("retrieved unexpected file: %s", got)
-		}
-	})
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			if len(tc.defaultPaths) > 0 {
+				configio.DefaultPaths = tc.defaultPaths
+			}
+			got := configio.FindFile(tc.inputPaths...)
+			if got != tc.exp {
+				t.Errorf("exp %q, got %q", tc.exp, got)
+			}
+		})
+	}
 }
 
 func TestUnmarshalFile(t *testing.T) {
-	supportedExt := []string{
-		".yml",
-		".yaml",
-		".json",
-	}
-
 	t.Run("return file errors early", func(t *testing.T) {
 		testcases := []struct {
 			label  string
-			path   string
+			file   testdata.ConfigFile
 			expErr error
 		}{
 			{
 				label:  "not found",
-				path:   configPath("invalid/bad path"),
+				file:   testdata.InvalidPath(),
 				expErr: configio.ErrFileNotFound,
 			},
 			{
 				label:  "unsupported extension",
-				path:   configPath("invalid/badext.yams"),
+				file:   testdata.InvalidExtension(),
 				expErr: configio.ErrFileExt,
 			},
 			{
 				label:  "yaml invalid fields",
-				path:   configPath("invalid/badfields.yml"),
+				file:   testdata.InvalidFieldsYML(),
 				expErr: configio.ErrFileParse,
 			},
 			{
 				label:  "json invalid fields",
-				path:   configPath("invalid/badfields.json"),
+				file:   testdata.InvalidFieldsJSON(),
 				expErr: configio.ErrFileParse,
 			},
 			{
 				label:  "self reference",
-				path:   configPath("extends/extends-circular-self.yml"),
+				file:   testdata.InvalidExtendsSelf(),
 				expErr: configio.ErrFileCircular,
 			},
 			{
 				label:  "circular reference",
-				path:   configPath("extends/extends-circular-0.yml"),
+				file:   testdata.InvalidExtendsCircular(),
 				expErr: configio.ErrFileCircular,
 			},
 		}
@@ -96,116 +100,74 @@ func TestUnmarshalFile(t *testing.T) {
 		for _, tc := range testcases {
 			t.Run(tc.label, func(t *testing.T) {
 				runner := benchttp.Runner{}
-				gotErr := configio.UnmarshalFile(tc.path, &runner)
+				err := configio.UnmarshalFile(tc.file.Path, &runner)
 
-				if gotErr == nil {
-					t.Fatal("exp non-nil error, got nil")
-				}
-
-				if !errors.Is(gotErr, tc.expErr) {
-					t.Errorf("\nexp %v\ngot %v", tc.expErr, gotErr)
-				}
-
-				if !sameRunner(runner, benchttp.Runner{}) {
-					t.Errorf("\nexp empty config\ngot %v", runner)
-				}
+				assertStaticError(t, tc.expErr, err)
+				benchttptest.AssertEqualRunners(t, tc.file.Runner, runner)
 			})
 		}
 	})
 
-	t.Run("happy path for all extensions", func(t *testing.T) {
-		for _, ext := range supportedExt {
-			filename := configPath("valid/benchttp" + ext)
-			runner := benchttp.Runner{}
+	t.Run("happy path all extensions", func(t *testing.T) {
+		for _, tc := range []struct {
+			name string
+			file testdata.ConfigFile
+		}{
+			{name: "full json", file: testdata.ValidFullJSON()},
+			{name: "full yaml", file: testdata.ValidFullYAML()},
+			{name: "full yml", file: testdata.ValidFullYML()},
+		} {
+			t.Run(tc.name, func(t *testing.T) {
+				runner := benchttp.Runner{}
+				err := configio.UnmarshalFile(tc.file.Path, &runner)
 
-			if err := configio.UnmarshalFile(filename, &runner); err != nil {
-				// critical error, stop the test
-				t.Fatal(err)
-			}
-
-			if sameRunner(runner, benchttp.Runner{}) {
-				t.Error("received an empty configuration")
-			}
-
-			exp := expectedRunner()
-			if !sameRunner(runner, exp) {
-				t.Errorf("unexpected parsed config for %s file:\nexp %#v\ngot %#v", ext, exp, runner)
-			}
-
+				mustAssertNilError(t, err)
+				benchttptest.AssertEqualRunners(t, tc.file.Runner, runner)
+			})
 		}
 	})
 
-	t.Run("override input config", func(t *testing.T) {
-		filename := configPath("valid/benchttp-zeros.yml")
-		runner := benchttp.Runner{}
-		runner.Request = httptest.NewRequest("POST", "https://overriden.com", nil)
-		runner.GlobalTimeout = 10 * time.Millisecond
-
-		if err := configio.UnmarshalFile(filename, &runner); err != nil {
-			t.Fatal(err)
+	t.Run("override dst with set config values", func(t *testing.T) {
+		cfg := testdata.ValidPartial()
+		runner := benchttp.Runner{
+			Request:       httptest.NewRequest("GET", "http://a.b", nil), // overridden
+			GlobalTimeout: 1 * time.Second,                               // overridden
 		}
 
-		const (
-			expMethod        = "POST"                // from input config
-			expGlobalTimeout = 42 * time.Millisecond // from read file
-		)
+		err := configio.UnmarshalFile(cfg.Path, &runner)
 
-		if gotMethod := runner.Request.Method; gotMethod != expMethod {
-			t.Errorf(
-				"did not keep input values that are not set: "+
-					"exp Request.Method == %s, got %s",
-				expMethod, gotMethod,
-			)
-		}
+		mustAssertNilError(t, err)
+		benchttptest.AssertEqualRunners(t, cfg.Runner, runner)
+	})
 
-		if gotGlobalTimeout := runner.GlobalTimeout; gotGlobalTimeout != expGlobalTimeout {
-			t.Errorf(
-				"did not override input values that are set: "+
-					"exp Runner.GlobalTimeout == %v, got %v",
-				expGlobalTimeout, gotGlobalTimeout,
-			)
-		}
+	t.Run("keep dst values not set in config", func(t *testing.T) {
+		const keptConcurrency = 5 // not set in config file
 
-		t.Log(runner)
+		cfg := testdata.ValidPartial()
+		exp := cfg.Runner
+		exp.Concurrency = keptConcurrency
+		dst := benchttp.Runner{Concurrency: keptConcurrency}
+
+		err := configio.UnmarshalFile(cfg.Path, &dst)
+
+		mustAssertNilError(t, err)
+		benchttptest.AssertEqualRunners(t, exp, dst)
 	})
 
 	t.Run("extend config files", func(t *testing.T) {
-		testcases := []struct {
-			label  string
-			cfname string
-			cfpath string
+		for _, tc := range []struct {
+			name string
+			cfg  testdata.ConfigFile
 		}{
-			{
-				label:  "same directory",
-				cfname: "child",
-				cfpath: configPath("extends/extends-valid-child.yml"),
-			},
-			{
-				label:  "nested directory",
-				cfname: "nested",
-				cfpath: configPath("extends/nest-0/nest-1/extends-valid-nested.yml"),
-			},
-		}
+			{name: "same directory", cfg: testdata.ValidExtends()},
+			{name: "nested directory", cfg: testdata.ValidExtendsNested()},
+		} {
+			t.Run(tc.name, func(t *testing.T) {
+				dst := benchttp.Runner{}
+				err := configio.UnmarshalFile(tc.cfg.Path, &dst)
 
-		for _, tc := range testcases {
-			t.Run(tc.label, func(t *testing.T) {
-				var runner benchttp.Runner
-				if err := configio.UnmarshalFile(tc.cfpath, &runner); err != nil {
-					t.Fatal(err)
-				}
-
-				var (
-					expMethod = "POST"
-					expURL    = fmt.Sprintf("http://%s.config", tc.cfname)
-				)
-
-				if gotMethod := runner.Request.Method; gotMethod != expMethod {
-					t.Errorf("method: exp %s, got %s", expMethod, gotMethod)
-				}
-
-				if gotURL := runner.Request.URL.String(); gotURL != expURL {
-					t.Errorf("url: exp %s, got %s", expURL, gotURL)
-				}
+				mustAssertNilError(t, err)
+				benchttptest.AssertEqualRunners(t, tc.cfg.Runner, dst)
 			})
 		}
 	})
@@ -213,87 +175,16 @@ func TestUnmarshalFile(t *testing.T) {
 
 // helpers
 
-// expectedRunner returns the expected benchttp.Runner after unmarhsaling
-// one of the valid config files in testdata.
-func expectedRunner() benchttp.Runner {
-	request := httptest.NewRequest(
-		"POST",
-		"http://localhost:9999?fib=30&delay=200ms",
-		bytes.NewReader([]byte(`{"key0":"val0","key1":"val1"}`)),
-	)
-	request.Header = http.Header{
-		"key0": []string{"val0", "val1"},
-		"key1": []string{"val0"},
-	}
-	return benchttp.Runner{
-		Request: request,
-
-		Requests:       100,
-		Concurrency:    1,
-		Interval:       50 * time.Millisecond,
-		RequestTimeout: 2 * time.Second,
-		GlobalTimeout:  60 * time.Second,
-
-		Tests: []benchttp.TestCase{
-			{
-				Name:      "minimum response time",
-				Field:     "ResponseTimes.Min",
-				Predicate: "GT",
-				Target:    80 * time.Millisecond,
-			},
-			{
-				Name:      "maximum response time",
-				Field:     "ResponseTimes.Max",
-				Predicate: "LTE",
-				Target:    120 * time.Millisecond,
-			},
-			{
-				Name:      "100% availability",
-				Field:     "RequestFailureCount",
-				Predicate: "EQ",
-				Target:    0,
-			},
-		},
+func mustAssertNilError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("exp nil error, got %v", err)
 	}
 }
 
-func sameRunner(a, b benchttp.Runner) bool {
-	if a.Request == nil || b.Request == nil {
-		return a.Request == nil && b.Request == nil
+func assertStaticError(t *testing.T, exp, got error) {
+	t.Helper()
+	if !errors.Is(got, exp) {
+		t.Errorf("unexpected error:\nexp %v\ngot %v", exp, got)
 	}
-	return sameURL(a.Request.URL, b.Request.URL) &&
-		reflect.DeepEqual(a.Request.Header, b.Request.Header) &&
-		reflect.DeepEqual(a.Request.Body, b.Request.Body)
-}
-
-// sameURL returns true if a and b are the same *url.URL, taking into account
-// the undeterministic nature of their RawQuery.
-func sameURL(a, b *url.URL) bool {
-	// check query params equality via Query() rather than RawQuery
-	if !reflect.DeepEqual(a.Query(), b.Query()) {
-		return false
-	}
-
-	// temporarily set RawQuery to a determined value
-	for _, u := range []*url.URL{a, b} {
-		restore := setTempValue(&u.RawQuery, "replaced by test")
-		defer restore()
-	}
-
-	// we can now rely on deep equality check
-	return reflect.DeepEqual(a, b)
-}
-
-// setTempValue sets *ptr to val and returns a restore func that sets *ptr
-// back to its previous value.
-func setTempValue(ptr *string, val string) (restore func()) {
-	previousValue := *ptr
-	*ptr = val
-	return func() {
-		*ptr = previousValue
-	}
-}
-
-func configPath(name string) string {
-	return filepath.Join("testdata", name)
 }
