9b8780039665a0a08ad4e6cbf01c60c275137e36
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-11-06 17:44:16 +0100

    doc(configio): write ExampleBuilder example

---
+++ configio/example_test.go
@@ -0,0 +1,40 @@
+package configio_test
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/benchttp/sdk/benchttp"
+	"github.com/benchttp/sdk/configio"
+)
+
+var jsonConfig = []byte(
+	`{"request": {"method": "GET", "url": "https://example.com"}}`,
+)
+
+var yamlConfig = []byte(
+	`{request: {method: PUT}, runner: {requests: 42}}`,
+)
+
+func ExampleBuilder() {
+	runner := benchttp.Runner{Requests: -1, Concurrency: 3}
+
+	b := configio.Builder{}
+	_ = b.WriteJSON(jsonConfig)
+	_ = b.WriteYAML(yamlConfig)
+	b.SetInterval(100 * time.Millisecond)
+
+	b.Into(&runner)
+
+	// Output:
+	// PUT
+	// https://example.com
+	// 42
+	// 3
+	// 100ms
+	fmt.Println(runner.Request.Method)
+	fmt.Println(runner.Request.URL)
+	fmt.Println(runner.Requests)
+	fmt.Println(runner.Concurrency)
+	fmt.Println(runner.Interval)
+}
